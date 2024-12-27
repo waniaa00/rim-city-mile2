dbab0af709cfac7bcdab714a5bfa05a01a886cdc
wania akram <160944972+waniaa00@users.noreply.github.com>
2024-12-27 16:22:20 +0500

    Update page.tsx

--- src/app/rims/page.tsx
+++ src/app/rims/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 
 const Rims = () => {
@@ -52,7 +53,7 @@ const Rims = () => {
     <div className='rims grid grid-cols-1 md:grid-cols-3 gap-10 px-10 my-10 '>
         {rimsData.map(item => (
             <div key={item.id} className='rim-card bg-white p-5 rounded-md shadow-md text-center'>
-                <img src={item.image} alt={item.name} className='w-full rounded-md transition-transform duration-300 transform hover:scale-110'/>
+                <Image src={item.image} alt={item.name} className='w-full rounded-md transition-transform duration-300 transform hover:scale-110'/>
                 <h3 className='mt-4 text-2xl font-bold'>{item.name}</h3>
                 <p className='text-gray-500'>{item.description}</p>
                 <div className='price text-blue-950 text-xl font-semibold mt-2'>${item.price}</div>
